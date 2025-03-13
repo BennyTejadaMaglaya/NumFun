f4c96c458103eb451d14c9595fb8186f1dce20c6
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-13 19:40:03 -0400

    Add <summary> as good practice.

--- NumFun/NumberFun.cs
+++ NumFun/NumberFun.cs
@@ -1,5 +1,11 @@
 ﻿namespace NumFun
 {
+    /// <summary>
+    /// Method to determine which number is the biggest
+    /// </summary>
+    /// <param name="first">First Number</param>
+    /// <param name="second">Second Number</param>
+    /// <return>Value of the biggest number</return>
     public static class NumberFun
     {
         public static int Biggest(int first, int second)
