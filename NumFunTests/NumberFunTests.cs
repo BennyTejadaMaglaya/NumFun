b520e8f3bfb9b093ebd9be6bb72dac5b3cefb561
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-13 19:46:54 -0400

    5.	Right-click on the Biggest method and select Create Unit Tests.

---
+++ NumFunTests/NumberFunTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumFun;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumFun.Tests
+{
+    [TestClass()]
+    public class NumberFunTests
+    {
+        [TestMethod()]
+        public void BiggestTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
