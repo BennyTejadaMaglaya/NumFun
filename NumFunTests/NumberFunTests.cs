73251fed595bbcd12a2cb6f6b411dbe3fe1e9d1b
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-13 19:57:03 -0400

    Add Arrange, Act, and Assert to the NumberFunTests' BiggestTestCase1.

--- NumFunTests/NumberFunTests.cs
+++ NumFunTests/NumberFunTests.cs
@@ -11,10 +11,22 @@ namespace NumFun.Tests
     [TestClass()]
     public class NumberFunTests
     {
+        /// <summary>
+        /// UseCase1 values: 5, 10, expected 10
+        /// </summary>
         [TestMethod()]
-        public void BiggestTest()
+        public void BiggestTestCase1()
         {
-            Assert.Fail();
+            // Arrange
+            int a = 5;
+            int b = 10;
+            int expected = 10;
+
+            // Act
+            int actual = NumberFun.Biggest(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
