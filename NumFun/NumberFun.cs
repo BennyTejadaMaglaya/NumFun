1f46df2e2b5776b5b7966281d4b8045b7b0e12dc
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-13 19:30:25 -0400

    Add the Biggest method.

--- NumFun/NumberFun.cs
+++ NumFun/NumberFun.cs
@@ -2,6 +2,16 @@
 {
     public static class NumberFun
     {
-
+        public static int Biggest(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
     }
 }
