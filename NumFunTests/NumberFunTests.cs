eff4ebe1fcb4ef37ffadb343aaa2ef8b51eac6d2
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-13 20:14:44 -0400

    Add BiggestTestCase2 and BiggestTestCase3.

--- NumFunTests/NumberFunTests.cs
+++ NumFunTests/NumberFunTests.cs
@@ -28,5 +28,41 @@ namespace NumFun.Tests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        /// <summary>
+        /// UseCase2 values: 10, 5, expected 10
+        /// </summary>
+        [TestMethod()]
+        public void BiggestTestCase2()
+        {
+            // Arrange
+            int a = 10;
+            int b = 5;
+            int expected = 10;
+
+            // Act
+            int actual = NumberFun.Biggest(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// UseCase3 values: 5, 5, expected 5
+        /// </summary>
+        [TestMethod()]
+        public void BiggestTestCase3()
+        {
+            // Arrange
+            int a = 5;
+            int b = 5;
+            int expected = 5;
+
+            // Act
+            int actual = NumberFun.Biggest(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
